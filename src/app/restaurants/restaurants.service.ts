67cb7bd6ed2c9861928ae1cd77fe18357068c3b6
Matheus S. Cardoso <matheusscardoso5@gmail.com>
2019-08-25 21:42:14 -0300

    start project

---
+++ src/app/restaurants/restaurants.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http } from "@angular/http";
+import { Restaurant } from './restaurant/restaurant.model';
+
+import {MEAT_API} from 'app/app.api';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class RestaurantsService {
+
+    constructor(private http : Http) { }
+
+    getRestaurants(): Observable<Restaurant[]>{
+        
+        return this.http.get(`${MEAT_API}/restaurants`)
+            .map(response => response.json())
+
+        // return this.http.get( MEAT_API + '/restaurants');
+    }
+
+}
