e55d604096f2b6ae5e79f4e3e2bf276c126eb177
Matheus S. Cardoso <matheusscardoso5@gmail.com>
2019-08-25 22:55:14 -0300

    update..

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -5,6 +5,8 @@ import { Restaurant } from './restaurant/restaurant.model';
 import {MEAT_API} from 'app/app.api';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { ErrorHandler } from 'app/app.error-handler';
 
 
 @Injectable()
@@ -12,10 +14,11 @@ export class RestaurantsService {
 
     constructor(private http : Http) { }
 
-    getRestaurants(): Observable<Restaurant[]>{
+    getRestaurants(): Observable<Restaurant[]>{ 
         
         return this.http.get(`${MEAT_API}/restaurants`)
             .map(response => response.json())
+            .catch(ErrorHandler.handleError)
 
         // return this.http.get( MEAT_API + '/restaurants');
     }
