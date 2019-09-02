922087d19c6e12e818f5ace98cf498b11ee2305c
Matheus S. Cardoso <matheusscardoso5@gmail.com>
2019-09-01 23:03:45 -0300

    restaurant-details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,24 @@
-import {Routes} from '@angular/router'
+import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 
 
-export const ROUTES : Routes =[
+export const ROUTES: Routes = [
 
-    {path: '', component: HomeComponent}
+    { path: '', component: HomeComponent }
     ,
-    
-    {path: 'about', component: AboutComponent}
+
+    { path: 'about', component: AboutComponent }
     ,
-    
-    {path: 'restaurants', component: RestaurantsComponent}
-    
-    
-    
+
+    { path: 'restaurants', component: RestaurantsComponent }
+    ,
+
+    { path: 'restaurants/:id ', component: RestaurantDetailComponent }
+
+
 
 
 ]
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,22 +2,27 @@ import { Injectable } from '@angular/core';
 import { Http } from "@angular/http";
 import { Restaurant } from './restaurant/restaurant.model';
 
-import {MEAT_API} from 'app/app.api';
+import { MEAT_API } from 'app/app.api';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-
 
 @Injectable()
 export class RestaurantsService {
 
-    constructor(private http : Http) { }
+    constructor(private http: Http) { }
 
-    getRestaurants(): Observable<Restaurant[]>{
-        
+    getRestaurants(): Observable<Restaurant[]> {
+
         return this.http.get(`${MEAT_API}/restaurants`)
-            .map(response => response.json())
+            .map(response => response.json());
+
 
         // return this.http.get( MEAT_API + '/restaurants');
+    }
+
+    getRestaurantById(id: string) : Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+            .map(response => response.json());
     }
 
 }
