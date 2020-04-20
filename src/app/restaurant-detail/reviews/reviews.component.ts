9d022d0e75daa380574e5bcec57021cfc8b305e5
Matheus S. Cardoso <matheusscardoso5@github.com>
2020-04-19 23:58:45 -0300

    reviews from "db"

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,4 +1,7 @@
+import { Observable } from 'rxjs/Observable'
+import { RestaurantsService } from './../../restaurants/restaurants.service'
 import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'mt-reviews',
@@ -6,8 +9,16 @@ import { Component, OnInit } from '@angular/core'
   styles: []
 })
 export class ReviewsComponent implements OnInit {
-  constructor () { }
+  reviews: Observable<any>
+
+  constructor (
+    private restaurantsService: RestaurantsService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit () {
+    const { id } =
+    this.activatedRoute.parent.snapshot.params
+
+    this.reviews = this.restaurantsService.reviewsOfRestaurant(id)
   }
 }
